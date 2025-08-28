4d97b849de437f04f621ab78c913b0c934ee4cfc
onestwob <onestwob@gmail.com>
2025-08-28 11:43:16 +0900

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -33,19 +33,15 @@ module tb ();
   wire VGND = 1'b0;
 `endif
     
-`ifdef GL_TEST
+    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
+    // include power ports for the Gate Level test
+    `ifdef GL_TEST
         //.VPWR( 1'b1),
         //.VGND( 1'b0),
         .VPWR( VPWR),
         .VGND( VGND),
-`endif
-
-    tt_um_seven_segment_seconds tt_um_seven_segment_seconds (
-    // include power ports for the Gate Level test
-    `ifdef GL_TEST
-        .VPWR( 1'b1),
-        .VGND( 1'b0),
     `endif
+
         .ui_in      (ui_in),    // Dedicated inputs
         .uo_out     (uo_out),   // Dedicated outputs
         .uio_in     (uio_in),   // IOs: Input path
